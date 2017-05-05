85084b62421deffe54dd81eb9aa99b8c81c59db8
pinkfloyd92 <ascacere92@gmail.com>
2017-05-05 09:15:17 -0500

    We can now get the dom from the announcements

--- scripts/login.js
+++ scripts/login.js
@@ -13,35 +13,21 @@ PASSWORD = arrContens[1];
 
 
 /**
- * Simple function to return the source path of all the scripts in the document
+ * Function that allows us to get the new announcements from the sidweb page.
  * of the <webview>
  *
- *@returns {String}
+ *@returns {Dom Element}
  **/
-function getScripts(){
-    var items = [];
 
-    for (var i = 0; i < document.scripts.length; i++){
-        items.push(document.scripts[i].src);
-    }
-
-    return JSON.stringify(items);
+function getTopics(){
+    return document.getElementsByClassName("announcement")[0].innerHTML;
 }
-
 
 // Do something according to a request of your mainview
 ipcRenderer.on("request", function(){
-    ipcRenderer.sendToHost(getScripts());
+    ipcRenderer.sendToHost(getTopics());
 });
 
-ipcRenderer.on("alert-something", function(event, data){
-    alert(data);
-});
-
-ipcRenderer.on("change-text-element", function(event, data){
-    // the document references to the document of the <webview>
-    document.getElementById(data.id).innerHTML = data.text;
-});
 
 window.onload = function() {
     var script = document.createElement("script");
--- scripts/sidweb.js
+++ scripts/sidweb.js
@@ -8,18 +8,19 @@ function init() {
 	if (this.getTitle().toString().includes("Actividad")) {
 	    console.info("DOM-Ready, triggering events !");
 	    webview.send("request");
-	    webview.send("alert-something", "Hey, i'm alerting this.");
-	    webview.send("change-text-element", {
-		id: "myelementID",
-		text: "My text"
-	    });
 	}
     });
 
     // Process the data from the webview
-    webview.addEventListener("ipc-message", function(event){
-	console.log(event);
-	console.info(event.channel);
+    webview.addEventListener("ipc-message", function(evt){
+	let message = evt.channel;
+	let parser = new DOMParser();
+	let element = parser.parseFromString(message, "text/html");
+	arrContents = element.getElementsByClassName("header");
+	arrContents = Array.from(arrContents);
+	arrContents = arrContents.slice(0, arrContents.length - 1);
+	console.log(arrContents[0].innerHTML);
+
     });
 }
 
